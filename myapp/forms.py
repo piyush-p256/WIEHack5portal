abbd4d36b72e515f32c8a4748663938d14769fc9
piyush-p256 <123575922+piyush-p256@users.noreply.github.com>
2024-04-22 19:48:58 +0530

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -9,7 +9,7 @@ class Round1Form(forms.ModelForm):
 class Round2UploadForm(forms.ModelForm):
     class Meta:
         model = Round2
-        fields = ['team_no', 'team_name', 'mode', 'member_name','member2', 'member3', 'member4', 'youtube_link', 'github_link2','ppt_link2']
+        fields = ['team_email','team_no', 'team_name', 'mode', 'member_name','member2', 'member3', 'member4', 'youtube_link', 'github_link2','ppt_link2']
 
 
 class Round3UploadForm(forms.ModelForm):
