b544d20fa4ec31c3676c9d799854ac00691eccde
piyush-p256 <123575922+piyush-p256@users.noreply.github.com>
2024-04-28 13:31:33 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -9,16 +9,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 django.setup()
 
 # Now you can import your models
-from myapp.models import Round2
+from myapp.models import Round1
 
 # Get data from Round3 model
-round2_data = Round2.objects.all().values()
+round1_data = Round1.objects.all().values()
 
 # Convert queryset to DataFrame
-df = pd.DataFrame(list(round2_data))
+df = pd.DataFrame(list(round1_data))
 
 # Define the Excel file path
-excel_file_path = 'round2_data.xlsx'
+excel_file_path = 'round1_data.xlsx'
 
 # Write DataFrame to Excel
 df.to_excel(excel_file_path, index=False)
